2f5d5f7f81fc0c6af3156097d24f6e4e2cf821c8
alexey.stepanov <neuromancer256@gmail.com>
2025-09-15 19:59:17 +0900

    Добавление моделей в domain

---
+++ NeuroCityBuilder/Assets/Domain/Gameplay/Building.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Gameplay
+{
+    public class Building
+    {
+        public BuildingType Type { get; set; }
+        public GridPos Position { get; set; }
+        public int CurrentLevel { get; set; }
+        public BuildingLevel[] Levels { get; set; }
+
+        public BuildingLevel GetCurrentLevelInfo() => this.Levels[this.CurrentLevel];
+    }
+}
---
+++ NeuroCityBuilder/Assets/Domain/Gameplay/GridPos.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Gameplay
+{
+    public class GridPos
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public GridPos(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is GridPos pos && this.X == pos.X && this.Y == pos.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.X.GetHashCode() ^ this.Y.GetHashCode();
+        }
+    }
+}
+
